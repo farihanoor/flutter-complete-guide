8b484791d6f0c38006d8d2c8aca6680f685a3601
farihanoor <fariha04noor@gmail.com>
2021-04-06 01:31:13 +0600

    elevatedButton added

--- flutter_app_basics/lib/main.dart
+++ flutter_app_basics/lib/main.dart
@@ -5,10 +5,40 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer Question!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite colour?',
+      'What\'s your favourite animal?',
+    ];
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+          
+        ),
+      ),
     );
   }
 }
